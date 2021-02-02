8e8252585c47828cf05173ac09482ad424d2bf8d
noob-G <iec2018055@iiita.ac.in>
2021-02-02 21:59:24 +0530

    ss

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -44,6 +44,14 @@ export default function HomeScreen() {
                 </Link>
               </div>
             ))}
+            {sellers.map((seller) => (
+              <div key={seller._id}>
+                <Link to={`/seller/${seller._id}`}>
+                  <img src={seller.seller.logo} alt={seller.seller.name} />
+                  <p className="legend">{seller.seller.name}</p>
+                </Link>
+              </div>
+            ))}
           </Carousel>
         </>
       )}
